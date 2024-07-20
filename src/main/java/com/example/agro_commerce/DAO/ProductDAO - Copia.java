30d50383a0ccef67c7a62d1cd7640ea96b8825a5
NathanDalbert <dalbert.nathan@gmail.com>
2024-07-20 15:50:41 -0300

    fix: corrigido metodos de inserir e buscar user

---
+++ src/main/java/com/example/agro_commerce/DAO/ProductDAO - Copia.java
@@ -0,0 +1,21 @@
+package com.example.agro_commerce.DAO;
+
+import com.example.agro_commerce.model.Product;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ProductDAO {
+
+    boolean insertProduct(Product product) throws SQLException;
+    List<Product> listAllProductsByName(String name);
+
+    List<Product> listAllProducts() throws SQLException;
+
+    boolean deleteProduct(Product product) throws SQLException;
+
+    boolean updateProduct(Product product) throws SQLException;
+
+    Product getProduct(int productId) throws SQLException;
+}
